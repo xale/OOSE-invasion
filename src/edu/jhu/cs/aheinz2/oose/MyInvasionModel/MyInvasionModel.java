0fd5dc4e2fde10890f095bb5b39a19de1b6d91e9
Alex Heinz <xale@acm.jhu.edu>
2010-09-01 16:03:14 -0400

    Added library .jar and Javadoc files, and skeletal InvasionModel implementation file.

---
+++ src/edu/jhu/cs/aheinz2/oose/MyInvasionModel/MyInvasionModel.java
@@ -0,0 +1,95 @@
+/**
+ * MyInvasionModel.java
+ * edu.jhu.cs.aheinz2.oose.MyInvasionModel
+ * @author alex_heinz
+ * Created on Sep 1, 2010
+ *
+ * InvasionModel-conforming model object implementation
+ */
+
+package edu.jhu.cs.aheinz2.oose.MyInvasionModel;
+
+import edu.jhu.cs.oose.fall2010.invasion.iface.IllegalMoveException;
+import edu.jhu.cs.oose.fall2010.invasion.iface.InvasionModel;
+import edu.jhu.cs.oose.fall2010.invasion.iface.InvasionModelListener;
+import edu.jhu.cs.oose.fall2010.invasion.iface.Location;
+import edu.jhu.cs.oose.fall2010.invasion.iface.Player;
+
+/**
+ * @author alex_heinz
+ *
+ */
+public class MyInvasionModel implements InvasionModel
+{
+	
+	/* (non-Javadoc)
+	 * @see edu.jhu.cs.oose.fall2010.invasion.iface.InvasionModel#addListener(edu.jhu.cs.oose.fall2010.invasion.iface.InvasionModelListener)
+	 */
+	@Override
+	public void addListener(InvasionModelListener listener)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/* (non-Javadoc)
+	 * @see edu.jhu.cs.oose.fall2010.invasion.iface.InvasionModel#endTurn()
+	 */
+	@Override
+	public void endTurn() throws IllegalMoveException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/* (non-Javadoc)
+	 * @see edu.jhu.cs.oose.fall2010.invasion.iface.InvasionModel#getCurrentPlayer()
+	 */
+	@Override
+	public Player getCurrentPlayer()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/* (non-Javadoc)
+	 * @see edu.jhu.cs.oose.fall2010.invasion.iface.InvasionModel#getPieceOwner(edu.jhu.cs.oose.fall2010.invasion.iface.Location)
+	 */
+	@Override
+	public Player getPieceOwner(Location location)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/* (non-Javadoc)
+	 * @see edu.jhu.cs.oose.fall2010.invasion.iface.InvasionModel#getWinner()
+	 */
+	@Override
+	public Player getWinner()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/* (non-Javadoc)
+	 * @see edu.jhu.cs.oose.fall2010.invasion.iface.InvasionModel#move(edu.jhu.cs.oose.fall2010.invasion.iface.Location, edu.jhu.cs.oose.fall2010.invasion.iface.Location)
+	 */
+	@Override
+	public void move(Location fromLocation, Location toLocation) throws IllegalMoveException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/* (non-Javadoc)
+	 * @see edu.jhu.cs.oose.fall2010.invasion.iface.InvasionModel#removeListener(edu.jhu.cs.oose.fall2010.invasion.iface.InvasionModelListener)
+	 */
+	@Override
+	public void removeListener(InvasionModelListener listener)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
