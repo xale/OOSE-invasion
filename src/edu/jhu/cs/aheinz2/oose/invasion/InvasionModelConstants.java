15c5fcd8ae435674f1d09a672820454b6041563e
Alex Heinz <xale@acm.jhu.edu>
2010-09-05 17:33:08 -0400

    [Cosmetic] Added some comments and whitespace to the constants file.

--- src/edu/jhu/cs/aheinz2/oose/invasion/InvasionModelConstants.java
+++ src/edu/jhu/cs/aheinz2/oose/invasion/InvasionModelConstants.java
@@ -6,6 +6,7 @@
  *
  * 
  */
+
 package edu.jhu.cs.aheinz2.oose.invasion;
 
 import edu.jhu.cs.oose.fall2010.invasion.iface.Location;
@@ -16,17 +17,21 @@ import edu.jhu.cs.oose.fall2010.invasion.iface.Location;
  */
 interface InvasionModelConstants
 {
+	// Board size
 	static final int INVASION_BOARD_WIDTH = 7;
 	static final int INVASION_BOARD_HEIGHT = INVASION_BOARD_WIDTH;
 	
+	// Cut-out corner sizes
 	static final int INVASION_BOARD_CORNER_WIDTH = 2;
 	static final int INVASION_BOARD_CORNER_HEIGHT = INVASION_BOARD_CORNER_WIDTH;
 	
+	// Bounding box on the fortress
 	static final int INVASION_BOARD_FORTRESS_MIN_X = 2;
 	static final int INVASION_BOARD_FORTRESS_MIN_Y = 4;
 	static final int INVASION_BOARD_FORTRESS_MAX_X = 4;
 	static final int INVASION_BOARD_FORTRESS_MAX_Y = 6;
 	
+	// Initial setup data
 	static final int INVASION_BOARD_NUM_PIRATE_OCCUPIED_ROWS = 4;
 	static final Location[] INVASION_BOARD_INITIAL_BULGAR_LOCATIONS =
 	{
@@ -34,16 +39,18 @@ interface InvasionModelConstants
 		new Location(4, 5)
 	};
 	
+	// Victory condition for bulgars
 	static final int INVASION_BOARD_MINIMUM_PIRATES_TO_WIN = 9;
 	
+	// Lookup table for fortress distances (-1 is off of board)
 	static final int[][] INVASION_BOARD_FORTRESS_DISTANCES =
 	{
-		{-1, -1, 2, 2, 2, -1, -1},
-		{-1, -1, 2, 1, 1, -1, -1},
-		{4, 3, 2, 1, 0, 0, 0},
-		{4, 3, 2, 1, 0, 0, 0},
-		{4, 3, 2, 1, 0, 0, 0},
-		{-1, -1, 2, 1, 1, -1, -1},
-		{-1, -1, 2, 2, 2, -1, -1}
+		{-1, -1,  2,  2,  2, -1, -1},
+		{-1, -1,  2,  1,  1, -1, -1},
+		{ 4,  3,  2,  1,  0,  0,  0},
+		{ 4,  3,  2,  1,  0,  0,  0},
+		{ 4,  3,  2,  1,  0,  0,  0},
+		{-1, -1,  2,  1,  1, -1, -1},
+		{-1, -1,  2,  2,  2, -1, -1}
 	};
 }
