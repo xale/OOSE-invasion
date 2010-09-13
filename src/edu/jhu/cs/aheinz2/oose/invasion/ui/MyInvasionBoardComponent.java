5b3c62ed718f0242179a7a091272642d1963fb94
Alex Heinz <xale@acm.jhu.edu>
2010-09-13 15:36:24 -0400

    Began writing the board component class, including support for swapping out the represented model.

--- src/edu/jhu/cs/aheinz2/oose/invasion/ui/MyInvasionBoardComponent.java
+++ src/edu/jhu/cs/aheinz2/oose/invasion/ui/MyInvasionBoardComponent.java
@@ -10,11 +10,52 @@ package edu.jhu.cs.aheinz2.oose.invasion.ui;
 
 import javax.swing.JComponent;
 
+import edu.jhu.cs.oose.fall2010.invasion.iface.*;
+
 /**
  * @author alex_heinz
  *
  */
 public class MyInvasionBoardComponent extends JComponent
 {
+	private InvasionModel model = null;
+	private InvasionModelListener modelListener = null;
 	
+	/**
+	 * Creates a new invasion board component, representing the specified model.
+	 * @param model The represented invasion model drawn by this component.
+	 */
+	public MyInvasionBoardComponent(InvasionModel model)
+	{
+		// Hold onto the model we've been given
+		this.model = model;
+		
+		// Create a listener that updates the component
+		this.modelListener = new InvasionModelListener()
+		{
+			@Override
+			public void receiveEvent(InvasionModelEvent event)
+			{
+				MyInvasionBoardComponent.this.repaint();
+			}
+		};
+		
+		// Attach the listener to the model
+		this.model.addListener(this.modelListener);
+	}
+	
+	public void setModel(InvasionModel newModel)
+	{
+		// Remove our listener from the old model
+		this.model.removeListener(this.modelListener);
+		
+		// Swap in the new model
+		this.model = newModel;
+		
+		// Add the listener to the new model
+		this.model.addListener(this.modelListener);
+		
+		// Repaint the component
+		this.repaint();
+	}
 }
