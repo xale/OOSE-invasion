b04b2303ae5d304542dc9e206332e371936dfb78
Alex Heinz <xale@acm.jhu.edu>
2010-09-13 13:30:51 -0400

    Added a listener from the UI on the model.

--- src/edu/jhu/cs/aheinz2/oose/invasion/MyInvasionUI.java
+++ src/edu/jhu/cs/aheinz2/oose/invasion/MyInvasionUI.java
@@ -23,6 +23,8 @@ public class MyInvasionUI extends JFrame
 	
 	public MyInvasionUI(Class<? extends InvasionModel> modelClass)
 	{
+		super();
+		
 		// Hold onto the class we're using for our model, so we can generate more if necessary
 		this.modelClass = modelClass;
 		
@@ -36,5 +38,44 @@ public class MyInvasionUI extends JFrame
 			// Invalid model class
 			throw new RuntimeException("Invalid model class:", e);
 		}
+		
+		// Add a listener to the model
+		model.addListener(new InvasionModelListener()
+		{
+			@Override
+			public void receiveEvent(InvasionModelEvent event)
+			{
+				if (event.isBoardChanged())
+					MyInvasionUI.this.observeBoardChanged();
+				if (event.isTurnChanged())
+					MyInvasionUI.this.observeTurnChanged();
+				if (event.isGameOver())
+					MyInvasionUI.this.observeGameEnded();
+			}
+		});
+	}
+	
+	/**
+	 * Called whenever an InvasionModelEvent informing us of a change in board state is received.  
+	 */
+	protected void observeBoardChanged()
+	{
+		// TODO Auto-generated method stub
+	}
+	
+	/**
+	 * Called whenever an InvasionModelEvent informing us of a turn change is received.
+	 */
+	protected void observeTurnChanged()
+	{
+		// TODO Auto-generated method stub
+	}
+	
+	/**
+	 * Called when an InvasionModelEvent indicating the end of a game is received.
+	 */
+	protected void observeGameEnded()
+	{
+		// TODO Auto-generated method stub
 	}
 }
