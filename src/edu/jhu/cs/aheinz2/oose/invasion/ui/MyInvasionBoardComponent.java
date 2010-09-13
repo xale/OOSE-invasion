5252cbcdf5f2f045918f74ccf457d9f4e3e23b94
Alex Heinz <xale@acm.jhu.edu>
2010-09-13 14:28:23 -0400

    Added a stub file for the custom component used to display the board.

---
+++ src/edu/jhu/cs/aheinz2/oose/invasion/ui/MyInvasionBoardComponent.java
@@ -0,0 +1,20 @@
+/**
+ * MyInvasionBoardComponent.java
+ * edu.jhu.cs.aheinz2.oose.invasion.ui
+ * @author alex_heinz
+ * Created on Sep 13, 2010
+ *
+ * 
+ */
+package edu.jhu.cs.aheinz2.oose.invasion.ui;
+
+import javax.swing.JComponent;
+
+/**
+ * @author alex_heinz
+ *
+ */
+public class MyInvasionBoardComponent extends JComponent
+{
+	
+}
