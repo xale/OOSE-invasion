d6771d47c708e9825b2ba4b2b79dd6b3b54b6f2f
Alex Heinz <xale@acm.jhu.edu>
2010-09-15 15:11:42 -0400

    Wrote the drawing code for the board component, including the loading of image files for the background and pieces.

--- src/edu/jhu/cs/aheinz2/oose/invasion/ui/MyInvasionBoardComponent.java
+++ src/edu/jhu/cs/aheinz2/oose/invasion/ui/MyInvasionBoardComponent.java
@@ -8,13 +8,14 @@
  */
 package edu.jhu.cs.aheinz2.oose.invasion.ui;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.geom.*;
+import java.net.URL;
 
 import javax.swing.JComponent;
+import javax.swing.ImageIcon;
 
-import edu.jhu.cs.aheinz2.oose.invasion.iface.InvasionConstants;
+import edu.jhu.cs.aheinz2.oose.invasion.iface.*;
 import edu.jhu.cs.oose.fall2010.invasion.iface.*;
 
 /**
@@ -25,6 +26,14 @@ public class MyInvasionBoardComponent extends JComponent
 {
 	private InvasionModel model = null;
 	private InvasionModelListener modelListener = null;
+	
+	private Location selectedPieceLocation = null;
+	
+	private ImageIcon boardImage = null;
+	private Dimension defaultBoardDimensions = new Dimension(600, 600);
+	
+	private ImageIcon invaderImage = null;
+	private ImageIcon defenderImage = null;
 	
 	/**
 	 * Creates a new invasion board component, initially with no represented model.
@@ -41,10 +50,20 @@ public class MyInvasionBoardComponent extends JComponent
 			}
 		};
 		
+		// Load image resources
+		// Board
+		this.boardImage = this.loadImageResource("Board.png");
+		
+		// Invader (pirate) piece
+		this.invaderImage = this.loadImageResource("Invader.png");
+		
+		// Defender (bulgar) piece
+		this.defenderImage = this.loadImageResource("Defender.png");
+		
 		// Set a default size for the component
-		this.setPreferredSize(new Dimension(500, 500));
+		this.setPreferredSize(this.defaultBoardDimensions);
 	}
-	
+
 	/**
 	 * Changes the model object that his component represents, and repaints the component accordingly.
 	 * @param newModel The new represented invasion model.
@@ -69,34 +88,78 @@ public class MyInvasionBoardComponent extends JComponent
 	@Override
 	protected void paintComponent(Graphics g)
 	{
-		// Paint the background black
-		g.setColor(Color.black);
-		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		// Copy the graphics context and cast to Graphics2D
+		Graphics2D g2d = (Graphics2D)g.create();
 		
-		// Paint the grid
-		for (int x = 0; x < InvasionConstants.INVASION_BOARD_WIDTH; x++)
-		{
-			for (int y = 0; y < InvasionConstants.INVASION_BOARD_HEIGHT; y++)
-			{
-				if (!InvasionConstants.coordinatesAreOnBoard(x, y))
-					continue;
-				
-				// TODO: WRITEME
-			}
-		}
+		// Scale the graphics context
+		g2d.scale((this.getWidth() / this.defaultBoardDimensions.getWidth()), (this.getHeight() / this.defaultBoardDimensions.getHeight()));
+		
+		// Draw the background
+		this.boardImage.paintIcon(this, g2d, 0, 0);
 		
 		// Draw the pieces
-		for (int x = 0; x < InvasionConstants.INVASION_BOARD_WIDTH; x++)
+		for (int col = 0; col < InvasionConstants.INVASION_BOARD_WIDTH; col++)
 		{
-			for (int y = 0; y < InvasionConstants.INVASION_BOARD_HEIGHT; y++)
+			for (int row = 0; row < InvasionConstants.INVASION_BOARD_HEIGHT; row++)
 			{
-				if (!InvasionConstants.coordinatesAreOnBoard(x, y))
+				// Check that these coordinates represent a valid board location
+				if (!InvasionConstants.coordinatesAreOnBoard(col, row))
 					continue;
 				
-				// TODO: WRITEME
-				// g.drawImage(img, x, y, width, height, null);
+				// Check if the location is occupied
+				Player pieceOwner = model.getPieceOwner(new Location(col, row));
+				if (pieceOwner == null)
+					continue;
+				
+				// Get the image corresponding to this player's piece
+				// TODO: check if piece is selected
+				ImageIcon pieceImage = this.pieceImageForPlayer(pieceOwner);
+				
+				// Find the point on the component corresponding to these board coordinates
+				int x = (int)((col + 1) * (this.defaultBoardDimensions.getWidth() / (InvasionConstants.INVASION_BOARD_WIDTH + 1)));
+				int y = (int)((row + 1) * (this.defaultBoardDimensions.getHeight() / (InvasionConstants.INVASION_BOARD_HEIGHT + 1)));
+				
+				// Center the image on the point
+				int drawX = x - (pieceImage.getIconWidth() / 2);
+				int drawY = y - (pieceImage.getIconHeight() / 2);
+				
+				// Draw the image
+				pieceImage.paintIcon(this, g2d, drawX, drawY);
 			}
 		}
+	}
+	
+	/**
+	 * Gets the piece image for a piece owned by the specified player.
+	 * @param pieceOwner The player who controls the piece.
+	 * @return the "invader" image if the player controls the pirates, or the "defender" image if the player controls the bulgars.
+	 */
+	private ImageIcon pieceImageForPlayer(Player pieceOwner)
+	{
+		switch (pieceOwner)
+		{
+			case PIRATE:
+				return this.invaderImage;
+			case BULGAR:
+				return this.defenderImage;
+			default:
+				break;
+		}
 		
+		throw new RuntimeException("Piece image requested for invalid player: " + pieceOwner);
+	}
+	
+	/**
+	 * Attempts to load the specified image file from the .../invasion/ui/resources/ directory.
+	 * @param imageName The name (with extension) of the image to load.
+	 * @return The image loaded from the file with the specified name.
+	 */
+	private ImageIcon loadImageResource(String imageName)
+	{
+		URL imageURL = this.getClass().getResource("resources/" + imageName);
+		if (imageURL == null)
+			throw new RuntimeException("Could not load resource files. Please ensure that " + imageName + " is in the edu/jhu/cs/aheinz2/oose/invasion/ui/resources/ directory.");
+		
+		return new ImageIcon(this.getToolkit().getImage(imageURL));
 	}
 }
