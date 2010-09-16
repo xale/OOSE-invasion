b62442f1f5fd2ccfc9b85cc19c5b7437820642d9
Alex Heinz <xale@acm.jhu.edu>
2010-09-15 22:06:28 -0400

    Added support for click-detection and hit-testing on the board components.

---
+++ src/edu/jhu/cs/aheinz2/oose/invasion/ui/InvasionClickListener.java
@@ -0,0 +1,24 @@
+/**
+ * MyInvasionClickListener.java
+ * edu.jhu.cs.aheinz2.oose.invasion.ui
+ * @author alex_heinz
+ * Created on Sep 15, 2010
+ *
+ * 
+ */
+package edu.jhu.cs.aheinz2.oose.invasion.ui;
+
+import edu.jhu.cs.oose.fall2010.invasion.iface.Location;
+
+/**
+ * @author alex_heinz
+ *
+ */
+public interface InvasionClickListener
+{
+	/**
+	 * Called when the component to which this listener is listening receives a click.
+	 * @param loc The location on the component's board of the click.
+	 */
+	public void locationClicked(Location loc);
+}
--- src/edu/jhu/cs/aheinz2/oose/invasion/ui/MyInvasionBoardComponent.java
+++ src/edu/jhu/cs/aheinz2/oose/invasion/ui/MyInvasionBoardComponent.java
@@ -9,7 +9,10 @@
 package edu.jhu.cs.aheinz2.oose.invasion.ui;
 
 import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
 import java.net.URL;
+import java.util.*;
 
 import javax.swing.JComponent;
 import javax.swing.ImageIcon;
@@ -26,22 +29,30 @@ public class MyInvasionBoardComponent extends JComponent
 	private InvasionModel model = null;
 	private InvasionModelListener modelListener = null;
 	
-	private Location selectedPieceLocation = null;
+	private Set<InvasionClickListener> clickListeners = new HashSet<InvasionClickListener>();
 	
 	private ImageIcon boardImage = null;
-	private Dimension defaultBoardDimensions = new Dimension(600, 600);
+	private static final Dimension DEFAULT_BOARD_DIMENSIONS = new Dimension(600, 600);
+	private static final double HORIZ_GRID_SPACING = (DEFAULT_BOARD_DIMENSIONS.getWidth() / (InvasionConstants.INVASION_BOARD_WIDTH + 1));
+	private static final double VERT_GRID_SPACING = (DEFAULT_BOARD_DIMENSIONS.getHeight() / (InvasionConstants.INVASION_BOARD_HEIGHT + 1));
 	
 	private ImageIcon invaderImage = null;
 	private ImageIcon selectedInvaderImage = null;
 	private ImageIcon defenderImage = null;
 	private ImageIcon selectedDefenderImage = null;
 	
+	private AffineTransform currentTransform = new AffineTransform();
+	
+	private Location selectedPieceLocation = null;
+	private static final double EMPTY_LOCATION_CLICK_RADIUS = 25.0;
+	private static final double OCCUPIED_LOCATION_CLICK_RADIUS = 35.0;
+	
 	/**
 	 * Creates a new invasion board component, initially with no represented model.
 	 */
 	public MyInvasionBoardComponent()
 	{
-		// Create a listener that updates the component, which will eventually be attached to a model object
+		// Create a listener for model events
 		this.modelListener = new InvasionModelListener()
 		{
 			@Override
@@ -50,6 +61,26 @@ public class MyInvasionBoardComponent extends JComponent
 				MyInvasionBoardComponent.this.repaint();
 			}
 		};
+		
+		// Create a listener for resize events
+		this.addComponentListener(new ComponentAdapter()
+		{
+			@Override
+			public void componentResized(ComponentEvent e)
+			{
+				MyInvasionBoardComponent.this.recalculateTransform();
+			}
+		});
+		
+		// Create a listener for mouse events
+		this.addMouseListener(new MouseAdapter()
+		{
+			@Override
+			public void mouseClicked(MouseEvent e)
+			{
+				MyInvasionBoardComponent.this.mouseClicked(e);
+			}
+		});
 		
 		// Load image resources
 		// Board
@@ -68,38 +99,43 @@ public class MyInvasionBoardComponent extends JComponent
 		this.selectedDefenderImage = this.loadImageResource("DefenderSelected.png");
 		
 		// Set a default size for the component
-		this.setPreferredSize(this.defaultBoardDimensions);
+		this.setPreferredSize(DEFAULT_BOARD_DIMENSIONS);
 	}
 
 	/**
-	 * Changes the model object that his component represents, and repaints the component accordingly.
-	 * @param newModel The new represented invasion model.
+	 * Called when the user clicks on the component; attempts to select/deselect at piece, or move the currently selected piece.
+	 * @param event The mouse event corresponding to the click. Should be a MOUSE_CLICKED event.
 	 */
-	public void setModel(InvasionModel newModel)
+	protected void mouseClicked(MouseEvent event)
 	{
-		// Remove our listener from the old model (if necessary)
-		if (this.model != null)
-			this.model.removeListener(this.modelListener);
+		// Check that this is a left-mouse (or button 1, for ambidexterity) event
+		if (event.getButton() != MouseEvent.BUTTON1)
+			return;
 		
-		// Swap in the new model
-		this.model = newModel;
+		// Get the coordinates of the click
+		Point2D clickPoint = null;
+		try
+		{
+			// Convert to the transformed coordinate system
+			clickPoint = currentTransform.inverseTransform(event.getPoint(), null);
+		}
+		catch (NoninvertibleTransformException e)
+		{
+			throw new RuntimeException("Non-invertible affine transform in mouseClicked()");
+		}
 		
-		// Add the listener to the new model
-		if (this.model != null)
-			this.model.addListener(this.modelListener);
-		
-		// Repaint the component
-		this.repaint();
+		// Map the clicked point to a location on the board, and inform listeners
+		this.sendClickEvent(this.locationAtPoint(clickPoint));
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics g)
 	{
 		// Copy the graphics context and cast to Graphics2D
 		Graphics2D g2d = (Graphics2D)g.create();
 		
-		// Scale the graphics context
-		g2d.scale((this.getWidth() / this.defaultBoardDimensions.getWidth()), (this.getHeight() / this.defaultBoardDimensions.getHeight()));
+		// Apply the current graphics transformation
+		g2d.setTransform(this.currentTransform);
 		
 		// Draw the background
 		this.boardImage.paintIcon(this, g2d, 0, 0);
@@ -123,8 +159,8 @@ public class MyInvasionBoardComponent extends JComponent
 				ImageIcon pieceImage = this.pieceImageForPlayer(pieceOwner, loc.equals(this.selectedPieceLocation));
 				
 				// Find the point on the component corresponding to these board coordinates
-				int x = (int)((col + 1) * (this.defaultBoardDimensions.getWidth() / (InvasionConstants.INVASION_BOARD_WIDTH + 1)));
-				int y = (int)((row + 1) * (this.defaultBoardDimensions.getHeight() / (InvasionConstants.INVASION_BOARD_HEIGHT + 1)));
+				int x = (int)((col + 1) * HORIZ_GRID_SPACING);
+				int y = (int)((row + 1) * VERT_GRID_SPACING);
 				
 				// Center the image on the point
 				int drawX = x - (pieceImage.getIconWidth() / 2);
@@ -134,6 +170,107 @@ public class MyInvasionBoardComponent extends JComponent
 				pieceImage.paintIcon(this, g2d, drawX, drawY);
 			}
 		}
+	}
+	
+	/**
+	 * Changes the model object that his component represents, and repaints the component accordingly.
+	 * @param newModel The new represented invasion model.
+	 */
+	public void setModel(InvasionModel newModel)
+	{
+		// Remove our listener from the old model (if necessary)
+		if (this.model != null)
+			this.model.removeListener(this.modelListener);
+		
+		// Swap in the new model
+		this.model = newModel;
+		
+		// Add the listener to the new model
+		if (this.model != null)
+			this.model.addListener(this.modelListener);
+		
+		// Repaint the component
+		this.repaint();
+	}
+	
+	/**
+	 * Adds a listener for clicks on this component.
+	 * @param listener The listener to add
+	 */
+	public void addClickListener(InvasionClickListener listener)
+	{
+		this.clickListeners.add(listener);
+	}
+	
+	/**
+	 * Sends a click event at the specified board location to all listeners.
+	 * @param clickedLocation The clicked location on the board.
+	 */
+	private void sendClickEvent(Location clickedLocation)
+	{
+		for (InvasionClickListener listener : this.clickListeners)
+			listener.locationClicked(clickedLocation);
+	}
+	
+	/**
+	 * Removes the specified listener from this component
+	 * @param listener The listener to remove.
+	 */
+	public void removeClickListener(InvasionClickListener listener)
+	{
+		this.clickListeners.remove(listener);
+	}
+	
+	/**
+	 * Recalculates the current affine transform for the component size.
+	 */
+	protected void recalculateTransform()
+	{
+		// Create a transform that maps the board to the largest possible centered square
+		if (this.getWidth() > this.getHeight())
+		{
+			// Wider than tall: shift horizontally to center
+			this.currentTransform.setToTranslation(((this.getWidth() - this.getHeight()) / 2), 0);
+			this.currentTransform.scale((this.getHeight() / DEFAULT_BOARD_DIMENSIONS.getWidth()), (this.getHeight() / DEFAULT_BOARD_DIMENSIONS.getHeight()));
+		}
+		else
+		{
+			// Taller than wide: shift vertically to center
+			this.currentTransform.setToTranslation(0, ((this.getHeight() - this.getWidth()) / 2));
+			this.currentTransform.scale((this.getWidth() / DEFAULT_BOARD_DIMENSIONS.getWidth()), (this.getWidth() / DEFAULT_BOARD_DIMENSIONS.getHeight()));
+		}
+	}
+	
+	/**
+	 * Attempts to map a clicked point on the component to a location on the board. 
+	 * @param clickPoint The clicked point, in the un-transformed coordinate system.
+	 * @return A location on the board, or null if no location is appropriate.
+	 */
+	private Location locationAtPoint(Point2D clickPoint)
+	{
+		// Find the nearest row and column on the board grid (note that these indexes may be off the board)
+		int nearestCol = ((int)Math.round(clickPoint.getX() / HORIZ_GRID_SPACING) - 1);
+		int nearestRow = ((int)Math.round(clickPoint.getY() / VERT_GRID_SPACING) - 1);
+		
+		// Check if the coordinates are on the board
+		if (!InvasionConstants.coordinatesAreOnBoard(nearestCol, nearestRow))
+			return null;
+		
+		// Determine if there is a piece at the nearest location to the click
+		Location nearestLocation = new Location(nearestCol, nearestRow);
+		Player pieceOwner = model.getPieceOwner(nearestLocation);
+		
+		// Determine how far away the click was from the coordinates
+		double distance = Math.hypot((((nearestCol + 1) * HORIZ_GRID_SPACING) - clickPoint.getX()), (((nearestRow + 1) * VERT_GRID_SPACING) - clickPoint.getY()));
+		
+		// If the location is unoccupied, use a small radius for click targets
+		if ((pieceOwner == null) && (distance <= EMPTY_LOCATION_CLICK_RADIUS))
+			return nearestLocation;
+		// If the location is occupied, try a larger click radius
+		else if ((pieceOwner != null) && (distance <= OCCUPIED_LOCATION_CLICK_RADIUS))
+			return nearestLocation;
+		
+		return null;
 	}
 	
 	/**
@@ -181,3 +318,4 @@ public class MyInvasionBoardComponent extends JComponent
 		return new ImageIcon(this.getToolkit().getImage(imageURL));
 	}
 }
+;
